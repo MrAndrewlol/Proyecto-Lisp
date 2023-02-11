afac431941cc640b6ee3a2437f504c0b23487bec
Andre <40283828+MrAndrewlol@users.noreply.github.com>
2023-02-10 21:57:13 -0600

    Actualización de Fibonacci

--- Fibonacci.scm
+++ Fibonacci.scm
@@ -2,6 +2,13 @@
 (define (fibonacci n)
   (if (<= n 2)
       1
+      
       (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
 
-(display (fibonacci 8))
+
+(display "Por favor ingresar el numero deseado: ")
+(define x (read))
+
+
+(display "el numero de Fibonacci es de: ")
+(display (fibonacci x))
