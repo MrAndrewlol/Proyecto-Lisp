05bf675efa82adab454fac6241a607d92d3482ab
Andre <40283828+MrAndrewlol@users.noreply.github.com>
2023-02-10 21:57:46 -0600

    Actualización de Farenheit

--- Farenheit_C.scm
+++ Farenheit_C.scm
@@ -1,18 +1,28 @@
 
+(define celcius 1.8)
+(define faren 0.566)
 
 (define (celcius_a_faren n)
   
-      (if (and (>= n -287)(<= n 100)) (+ 32 (* n 1.8 )) (+ n 0))
+      (if (and (>= n -287)(<= n 100)) (+ 32 (* n celcius )) (+ n 0))
 
         )
-
-(display (celcius_a_faren 20))
-
 
 (define (faren_a_celcius n)
   
       (if (and (>= n -459)(<= n 212)) (* 0.566 (- n 32 )) (+ n 0))
 
         )
+
+(display "Por favor ingresar grados en Celcius: ")
+(define x (read))
+(display "Por favor ingresar grados en Farenheit ")
+(define s (read))
+'(newline)
+
+
+(display "La conversion entre Celcius y Farenheit es de: ")
+(display (celcius_a_faren x))
 (newline)
-(display (faren_a_celcius 5))
+(display "La conversion entre Farenheit y Celcius es de: ")
+(display (faren_a_celcius s))
