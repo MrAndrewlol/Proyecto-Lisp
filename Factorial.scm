1b6b06b88c0087e0183a5fdb25c8c8be275fed04
Andre <40283828+MrAndrewlol@users.noreply.github.com>
2023-02-10 21:58:10 -0600

    Actualización de Factorial

--- Factorial.scm
+++ Factorial.scm
@@ -1,8 +1,12 @@
 
-
 (define (factorial n)
-  (if (= n 0)
-	  1
-	  (* n (factorial (- n 1)))))
+  (if (= n 0)1(* n (factorial (- n 1)))))
+(define s 0)
 
-(display (factorial 3))
+(display "Por favor ingresar un numero para realizar el factorial ")
+(set! s (read))
+ 
+
+(display "El numero factorial es: ")
+(display (factorial s))
+
