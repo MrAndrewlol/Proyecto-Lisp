962ddafdfc7b062cbad14d46cf318137fba77ad3
Andre <40283828+MrAndrewlol@users.noreply.github.com>
2023-02-10 20:44:22 -0600

    Error upload en el anterior

--- Fibonacci.scm
+++ Fibonacci.scm
@@ -1,18 +1,8 @@
-#lang scheme
+ #lang racket
 
-(define (celcius_a_faren n)
-  
-      (if (and (>= n -287)(<= n 100)) (+ 32 (* n 1.8 )) (+ n 0))
+(define (fibonacci n)
+  (if (<= n 2)
+      1
+      (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
 
-        )
-
-(display (celcius_a_faren 20))
-
-
-(define (faren_a_celcius n)
-  
-      (if (and (>= n -459)(<= n 212)) (* 0.566 (- n 32 )) (+ n 0))
-
-        )
-(newline)
-(display (faren_a_celcius 5))
+(display (fibonacci 8))
