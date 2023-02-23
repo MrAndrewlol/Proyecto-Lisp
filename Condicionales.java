13404d221bc59618ed077421e2b20c8391947c52
Andre <Andrewlol774@gmail.com>
2023-02-23 09:22:26 -0600

    Creacion de la clase condicionales
    Signed-off-by: Andre <Andrewlol774@gmail.com>

---
+++ Condicionales.java
@@ -0,0 +1,45 @@
+
+public class Condicionales{
+      private String Condicion;
+      
+  
+      public Condicionales(){
+            Condicion = "";
+            
+      }
+  
+  
+  
+  public Condicionales(String Condicion){
+      this.Condicion = Condicion;
+   
+  }
+  
+
+    
+    /** 
+     * @return String
+     */
+    public String getCondicion() {
+        return this.Condicion;
+    }
+
+    public void setCondicion(String Condicion) {
+        this.Condicion = Condicion;
+    }
+
+    public boolean siescondicional( String Condition){
+        boolean condicional
+        if(Condicion) 
+        
+        
+        return false;
+        
+    }
+  
+  
+  
+  }
+  
+      
+  
