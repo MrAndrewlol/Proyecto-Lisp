7004a553d8168b86b8129e6d6bbfb0f92143c602
Andre <Andrewlol774@gmail.com>
2023-02-23 09:08:41 -0600

    Se creo la clase Condicionales
    Signed-off-by: Andre <Andrewlol774@gmail.com>

---
+++ Condicionales.java
@@ -0,0 +1,36 @@
+
+public class Condicionales{
+      private String Condicion;
+      
+  
+      public Condicionales(){
+            Condicion = "";
+            
+      }
+  
+  
+  
+  public Condicionales(String owners, String numero, float dinero){
+      this.Condicion = Condicion;
+   
+  }
+  
+
+    
+    /** 
+     * @return String
+     */
+    public String getCondicion() {
+        return this.Condicion;
+    }
+
+    public void setCondicion(String Condicion) {
+        this.Condicion = Condicion;
+    }
+  
+  
+  
+  }
+  
+      
+  
