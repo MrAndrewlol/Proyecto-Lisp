143c727b2220871063e3b0805754f2eefa2bb76d
Jose-Prince <joseprince148mail.com>
2023-03-01 18:37:56 -0600

    Implementación suma
    Se implementa la suma para el lenguaje lisp

--- code/Aritmetica.java
+++ code/Aritmetica.java
@@ -1,35 +1,73 @@
 public class Aritmetica {
 
-    Stack<Integer> Stack = new Stack<Integer>();
+    Stack<String> stack = new Stack<String>();
 
     public String operar(String codigo) {
 
         codigo = new StringBuilder(codigo).reverse().toString();
-        
-        for (int i = 0; i < codigo.length(); i++){
+        char[] chars = codigo.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        String s = "";
+        String num = "";
+        int resultado = 0;
 
+        for (char c : chars) {
+            if (Character.isDigit(c)){
+                s = String.valueOf(c);
+                num = s + num;
+            } else if (c == ' ') {
+                stack.push(num);
+                num = "";
+            } else if (c == ')') {
+                s = String.valueOf(c);
+                stack.push(s);
+
+            } else {
+                if (c == '+') {
+                    suma(stack, resultado);
+                } else if (c == '-') {
+
+                } else if (c == '*') {
+
+                } else if (c == '/'){
+
+                }
+            }
         }
+        
 
         return codigo;
     }
     
-    public int suma(String codigo) {
+    public int suma(Stack<String> stack, int resultado) {
+        boolean ejecucion = true;
+        while (ejecucion) {
+            String cha = stack.peek();
+            if (cha.matches("[0-999]*")){
+                int valor = Integer.valueOf(stack.peek());
+                stack.pull();
+                resultado = valor + resultado;
+            } else {
+                ejecucion = false;
+            }
+        }
+        stack.pull();
+        return resultado;
+    }
+
+    public int resta(Stack<String> stack, int resultado) {
         return 0;
     }
 
-    public int resta(String codigo) {
+    public int multiplicacion(Stack<String> stack, int resultado) {
         return 0;
     }
 
-    public int multiplicacion(String codigo) {
+    public int division(Stack<String> stack, int resultado) {
         return 0;
     }
 
-    public int division(String codigo) {
-        return 0;
-    }
-
-    public int modulo(String codigo) {
+    public int modulo(Stack<String> stack, int resultado) {
         return 0;
     }
 
